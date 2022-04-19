a7d61e3a796672f5a5cda64ecb7c5428672775fe
thek1 <iamkeyvan72@gmail.com>
2022-04-19 12:35:55 +0430

    Add selectors

---
+++ src/todos/TodoListItem/styles.js
@@ -0,0 +1,18 @@
+import Styled from 'styled-components';
+
+export const StyledTodoListItem = Styled.div`
+ background: #fff;
+    border-radius: 8px;
+    margin-top: 8px;
+    padding: 16px;
+    position: relative;
+    box-shadow: 0 4px 8px grey;
+    `;
+
+export const StyledButtonsContainer = Styled.div`
+    position: absolute;
+    right: 12px;
+    bottom: 12px;
+    `;
+
+    export const 
